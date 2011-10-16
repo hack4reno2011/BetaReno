eb1387207687d7d13ed45b188bd2f86aaa73b10f
Dylan Kuhn <dylankkuhn@gmail.com>
2011-10-16 04:54:22 -0700

    Added the Google Places library.

---
+++ wordpress/wp-content/themes/thematic-child/geo-mashup-map-frame.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Individual registered scripts can be added with code like
+ *
+ * <code>GeoMashupRenderMap::enqueue_script( 'colorbox' );</code>
+ *
+ * Or include all queued resources by replacing
+ *
+ * <code>GeoMashupRenderMap::head();</code>
+ *
+ * with
+ *
+ * <code>wp_head();</code>
+ *
+ * @package GeoMashup
+ */
+
+wp_register_script( 'googlev3-places', 'http://maps.googleapis.com/maps/api/js?libraries=places&sensor=false', array( 'google-maps-3' ) );
+GeoMashupRenderMap::enqueue_script( 'googlev3-places' );
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+		"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<!--[if lte IE 6]>
+<html xmlns:v="urn:schemas-microsoft-com:vml">
+<![endif]-->
+
+	<head>
+		<meta http-equiv="content-type" content="text/html; charset=utf-8"/>
+	<title>Geo Mashup Map</title>
+		<?php GeoMashupRenderMap::head(); ?>
+
+		<style type="text/css">
+			v\:* { behavior:url(#default#VML); }
+			#geo-mashup {
+				width:<?php echo GeoMashupRenderMap::map_property( 'width' ); ?>;
+				height:<?php echo GeoMashupRenderMap::map_property( 'height' ); ?>;
+			}
+		</style>
+	</head>
+	<body>
+	<div id="geo-mashup">
+		<noscript>
+			<p><?php _e( 'This map requires JavaScript. You may have to enable it in your settings.', 'GeoMashup' ); ?></p>
+		</noscript>
+	</div>
+	<?php echo GeoMashupRenderMap::map_script( 'geo-mashup' ); ?>
+	</body>
+</html>
